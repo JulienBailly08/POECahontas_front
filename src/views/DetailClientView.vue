2a5caacf70b1b83c5989d1183ba0b1ae777b272a
Julien B <julienbailly08@gmail.com>
2023-12-29 15:08:36 +0100

    preparation affichage chargement

--- src/views/DetailClientView.vue
+++ src/views/DetailClientView.vue
@@ -1,24 +1,31 @@
 <template>
-  <p>{{ client }}</p>
-  <hr />
-  <p>Adresse</p>
-  <p>{{ client.address }}</p>
-  <hr />
-  <p>Entreprise</p>
-  <p>{{ client.company }}</p>
+  <div v-if="waitDatas">TEST</div>
+
+  <div v-if="!waitDatas">
+    <p>{{ client }}</p>
+    <hr />
+    <p>Adresse</p>
+    <p>{{ client.address }}</p>
+    <hr />
+    <p>Entreprise</p>
+    <p>{{ client.company }}</p>
+  </div>
 </template>
 <script>
 export default {
   name: "DetailClientView",
   data: function () {
     return {
+      waitDatas: Boolean,
       client: {},
     };
   },
   mounted() {
+    this.waitDatas = true;
     fetch("https://jsonplaceholder.typicode.com/users/" + this.$route.params.id)
       .then((res) => res.json())
       .then((data) => {
+        this.waitDatas = false;
         this.client = data;
       });
   },
